1cfade5be8f4adc3dbf905ea561af2ca046ed239
amir.h.ashkbar <amir.h.ashkbar@gmail.com>
2025-01-31 21:39:23 +0330

    add weather api and loading

---
+++ src/app/api/weatherApi.ts
@@ -0,0 +1,1 @@
+export const api = `https://api.openweathermap.org/data/2.5/forecast?q=tehran&appid=${process.env.NEXT_PUBLIC_WEATHER_KEY}&cnt=2`;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,33 @@
-import Image from "next/image";
+"use client";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "./api/weatherApi";
 import Navbar from "./components/Navbar";
+import axios from "axios";
 
 export default function Home() {
-  return (
-    <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
-      <Navbar/>
+  const { isPending, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: async () => {
+      try {
+        const response = await axios.get(api);
+        return response.data;
+      } catch (err) {
+        throw new Error("Failed to fetch data" + err);
+      }
+    },
+  });
+  console.log(data);
+  if (isPending)
+    return (
+      <div className="flex items-center min-h-screen justify-center">
+        <div className="w-8 h-8 border-4 border-gray-300 border-t-blue-500 rounded-full animate-spin"></div>
       </div>
+    );
+  return (
+    <>
+      <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
+        <Navbar />
+      </div>
+    </>
   );
 }
